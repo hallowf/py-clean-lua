e57c5bab054d3435f4c7e3eb8a74e9bfed669039
hallowf <alexandre.cortegaca@gmail.com>
2019-01-19 19:19:01 +0000

    added config parser, added more args, added readme

---
+++ README.md
@@ -0,0 +1,26 @@
+
+### Parameters
+
+- -ugl/--ugliffy
+  * Replaces new line characters "\n" with spaces
+- -clW/--clearWhites
+  * Removes excess whitespace
+- -remG/--removeGlobals
+  * Removes globals defined
+  * Pass in a file name trough the command-line or config
+  * the file must be in the root folder as the program
+- -uC/--useConfig
+  * Use configuration file see [Default Config file](#-default-config-file)
+
+### Default Config file
+
+```
+[DEFAULT]
+clearWhites = True
+removeGlobals = .lua_globals
+ugglify = False
+toRem = Debugging
+        SomeOtherGlobal
+        SomeOtherLocal
+
+```
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import os, traceback, shutil, time
+import os, traceback, shutil, time,argparse, configparser
 
-to_walk = {}
-G_to_ignore = ["Debugging", "print"]
-uggliffy = False
+G_to_rem = []
 clear_whites = False
-ugl = None
+ugl = "\n"
 
+### Args
+arg_parser = argparse.ArgumentParser("create_build_lua")
+arg_parser.add_argument("-ugl","--ugglify", help="Replaces new lines with spaces", action="store_true")
+arg_parser.add_argument("-clW", "--clearWhites", help="Removes excess whitespaces", action="store_true")
+arg_parser.add_argument("-remG", "--removeGlobals", help="Removes defined globals, needs a file", action="store")
+arg_parser.add_argument("-uC", "--useConfig", help="Use configuration file, needs a file", action="store")
+arg_parser.add_argument("-svC", "--saveConfig", help="Save current params to config file", action="store")
 
+### Config
+config = configparser.ConfigParser(allow_no_value=True)
 
 # Custom class to handle file reading
 class FileHandler():
@@ -68,7 +75,7 @@ def check_and_return_line(line):
     clr_line = line.strip()
   if clr_line != "":
     #line_strt = clr_line.split()[0]
-    for L_global in G_to_ignore:
+    for L_global in G_to_rem:
       if clr_line.startswith(L_global):
         if "{" in clr_line:
           print("Cant handle tables")
@@ -93,12 +100,45 @@ def check_and_return_line(line):
         return clr_line + ugl
   else:
     return ""
-    
+
+def check_and_set_args(args):
+  if args.useConfig:
+    if os.path.isfile(args.useConfig):
+      config.read(args.useConfig)
+      if config["DEFAULT"]["clearWhites"]:
+        clear_whites = True
+      if config["DEFAULT"]["removeGlobals"]:
+        G_to_rem = config.get("DEFAULT", "toRem").split("\n")
+      if config["DEFAULT"]["ugglify"]:
+        ugl = " "
+    else:
+      raise FileNotFoundError
+  if args.ugglify:
+    ugl = " "
+  if args.clearWhites:
+    clear_whites = True
+  if args.removeGlobals:
+    if os.path.isfile(args.removeGlobals):
+      conf = FileHandler(args.removeGlobals, "r")
+      with conf as c:
+        vals = c.read()
+        G_to_rem = [val for val in vals.split()]
+    else:
+      raise FileNotFoundError
+  if args.saveConfig:
+    config["DEFAULT"] = {
+      "clearWhites": clear_whites,
+      "removeGlobals": args.removeGlobals,
+      "uggliffy": args.ugglify,
+      "toRem": G_to_rem
+    }
+    conf_file = FileHandler(args.saveConfig, "w")
+    with conf_file as c:
+      config.write(c)
 
 
 if __name__ == "__main__":
-  if uggliffy:
-    ugl = " "
-  else:
-    ugl = "\n"
-  start_main()
+  args = arg_parser.parse_args()
+  check_and_set_args(args)
+  
+  #start_main()
